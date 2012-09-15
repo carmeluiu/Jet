ff5d113832ceee241215815eccdd652c985bf029
carmel <carmelzuk@gmail.com>
2012-09-15 22:55:38 +0300

    add HookList.java

---
+++ HookList.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jet;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author carmel
+ */
+public class HookList extends HashMap<String, Module> {
+    
+}
